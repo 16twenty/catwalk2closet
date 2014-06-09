6269cbd0274f4ea705d66586c7782bd4b88c7794
Sburt90 <burtscot@gmail.com>
2014-06-09 15:11:48 -0400

    missing dir's added. Basic page templates added.

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact | High Speed Internet</title>
+        @include('partials/css')
+    </head>
+    <body>
+        @include('partials/nav')
+        
+        @yield('content')
+        
+        @include('partials/footer')
+    </body>
+</html>
---
+++ app/views/partials/css.blade.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="{{ asset('gumby/css/gumby.css') }}" />
