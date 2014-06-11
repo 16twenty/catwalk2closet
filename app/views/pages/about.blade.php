08548d1a15494fa2d8f2b2b18dcb7014485badba
tushar-basu <basu.tushar@gmail.com>
2014-06-11 15:23:26 -0400

    new page content, vip styles

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.default')
 
 @section('content')
+<div class="event">
+	<div class="row">
+		<div class="six columns centered">
+			<div class="ribbon">
+				<h2>About the Show</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="ten columns centered">
+			<img class="bordered" src="{{ asset('img/POOL_trade_show_feb12_HypeType-3_2000.jpg') }}">
+		</div>
+	</div>
+	<div class="row">
+		<div class="ten columns centered">
+			<p><strong>Nunc cras non</strong>, ultrices pulvinar augue. Turpis etiam enim in? Phasellus cras? Vel ultrices sociis et, elementum turpis. Augue nunc, vut augue mauris porta. Porttitor natoque sagittis adipiscing non dignissim! Ac enim nunc phasellus! Placerat hac? Natoque integer dis. <a href="#">Natoque</a> duis, egestas pid lorem nascetur. </p>
+		</div>
+	</div>
+	<div class="row" style="margin-top:30px;">
+		<div class="ten columns centered">
+			<div class="promo text-center">
+				<h4>"Toronto's only premier designer sample sale and tradeshow"</h4>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="ten columns centered">	
+			<p>Magnis adipiscing mid, nec vel. Et mattis massa vel, augue sed integer magnis a integer lacus scelerisque enim? Aliquet cursus natoque, lacus duis vut, pellentesque. <strong>Porta nisi egestas aliquam tristique</strong>, amet quis dapibus sociis urna! Scelerisque augue, parturient ridiculus purus! Tortor porta enim et aenean, sit, magna, cum dolor in duis, mid <a href="#">adipiscing</a>, nisi quis, nisi cursus magna rhoncus ridiculus adipiscing, ut? Lectus eu et sed auctor lectus odio turpis lectus? Hac placerat, eros.</p>
 
-About the Show
+			<p style="padding-bottom:30px;">Quis. Nisi, ultricies et. Integer? Dictumst urna scelerisque mid? Rhoncus adipiscing? Vut nec, amet! Turpis? Augue? Tincidunt turpis scelerisque, augue facilisis sagittis sed rhoncus. Habitasse, elementum facilisis augue tincidunt, sed risus ultricies lundium nascetur! Ultrices augue et adipiscing mattis sed ridiculus ac mid magnis in sagittis ut, adipiscing penatibus pulvinar ac duis a, nunc placerat magna enim, <strong>nisi porttitor aenean et!</strong> A? Enim rhoncus purus elementum? Rhoncus, magnis porttitor, urna placerat? A pellentesque, pulvinar facilisis enim nascetur enim habitasse? Porta phasellus proin? <a href="#">Massa lorem</a> hac vel duis, et platea, aenean dolor penatibus! Proin? Aenean. Mid enim vel mattis ultrices, rhoncus porttitor a penatibus, mus sit dapibus rhoncus, pellentesque velit, auctor. Integer, turpis vel mus ac cras egestas, dapibus elit.</p>
+		</div>
+	</div>
+</div>
 @include('content.contact')
 @stop
--- app/views/pages/vip.blade.php
+++ app/views/pages/vip.blade.php
@@ -2,44 +2,33 @@
 
 @section('content')
 <div class="event">
-<div class="row">
-	<div class="six columns centered">
-		<div class="ribbon">
-			<h2>Get On The VIP List</h2>
+	<div class="row">
+		<div class="six columns centered">
+			<div class="ribbon">
+				<h2>Get On The VIP List</h2>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="row">
-    <div class="eight columns centered">
-        VIPs get early access to the event so they can snag the best deals before anyone else. Want to get on the list? Fill out the form below.
-        <form name="viplistform" action="{{ URL::to('vip') }} " method="post">
-            <ul>
-                <li class="field">
-                    <label class="inline" for="firstname" >First Name:</label>
-                    <input class="wide text input" type="text" name="firstname" id="firstname" placeholder="First Name" />
-                </li>
-                <li class="field">
-                    <label class="inline" for="lastname" >Last Name:</label>
-                    <input class="wide text input" type="text" name="lastname" id="lastname" placeholder="Last Name" />
-                </li>
-                <li class="field">
-                    <label class="inline" for="email" >Email Address:</label>
-                    <input class="wide text input" type="text" name="email" id="email" placeholder="Email Address" />
-                </li>
-                <li>
-                    <div class="medium primary btn">
-                        <input type="submit" value="submit" />
-                    </div>
-                </li>
-            </ul>
-        </form>
-    </div>
-</div>
-<div class="row">
-    <div class="six columns centered">
-        Thank you for your interest in Catwalk 2 Closet!
-    </div>
-</div>
+	<div class="row">
+	    <div class="eight columns centered text-center">
+	        <strong>VIPs</strong> get exclusive access to the showroom on opening night so they can snag the best deals before anyone else. <strong>Want to get on the list?</strong> Fill out the form below.
+	        <form class="vip_list nine columns centered" name="viplistform" action="{{ URL::to('vip') }} " method="post">
+	            <h3 class="text-center"><i class="icon-star"></i>Become a VIP</h3>
+	            <ul>
+	                <li class="field text-center">
+	                    <input class="wide text input" type="text" name="firstname" id="firstname" placeholder="First Name" />
+	                </li>
+	                <li class="field text-center">
+	                    <input class="wide text input" type="text" name="lastname" id="lastname" placeholder="Last Name" />
+	                </li>
+	                <li class="append field text-center">
+	                    <input class="wide text input" type="email" name="email" id="email" placeholder="Email Address" />
+	                    <div class="medium primary btn"><input type="submit" value="Go"/></div>
+	                </li>
+	            </ul>
+	        </form>
+	    </div>
+	</div>
 </div>
 
 @include('content.contact')
