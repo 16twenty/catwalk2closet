250b0463c7adf57c790dbb2ea0e14016dc6b36ee
Sburt90 <burtscot@gmail.com>
2014-06-12 15:51:39 -0400

    sell Sheet added

---
+++ app/views/content/sellsheet.blade.php
@@ -0,0 +1,7 @@
+<div class="row">
+	<div class="cta">
+		<h3>Become an Exhibitor Now!</h3>
+		<p>With over 30K attendees anticipated to make their way to our <strong>FIRST EVER CATWALK <span class="big_two">2</span> CLOSET SAMPLE SALE</strong>, we are the best place in Toronto to show off your goods! Have a look at our <strong>SELL SHEET</strong> below and contact us for pricing and details! </p>
+		<div class="medium default btn icon-left entypo icon-map"><a href="{{ asset('/downloads/C2C_info_sheet.pdf') }}">SELL SHEET</a></div>
+	</div>
+</div>
--- app/views/pages/exhibitors.blade.php
+++ app/views/pages/exhibitors.blade.php
@@ -9,13 +9,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="cta">
-			<h3>Become an Exhibitor Now!</h3>
-			<p>With over 30K attendees anticipated to make their way to our <strong>FIRST EVER CATWALK <span class="big_two">2</span> CLOSET SAMPLE SALE</strong>, we are the best place in Toronto to show off your goods! Have a look at our <strong>SELL SHEET</strong> below and contact us for pricing and details! </p>
-			<div class="medium default btn icon-left entypo icon-map"><a href="#">SELL SHEET</a></div>
-		</div>
-	</div>
+	@include('content.sellsheet')
 	<div class="row" style="margin-top:30px;">
 		<div class="ten columns centered">
 			<div class="promo text-center">
