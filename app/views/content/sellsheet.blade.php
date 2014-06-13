b72c7cd462159075f1f34d886ec5733f905abd41
tushar-basu <basu.tushar@gmail.com>
2014-06-13 13:54:51 -0400

    changes from lynda

--- app/views/content/sellsheet.blade.php
+++ app/views/content/sellsheet.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="cta">
-		<h3>Become an Exhibitor Now!</h3>
-		<p>With over 30K attendees anticipated to make their way to our <strong>FIRST EVER CATWALK <span class="big_two">2</span> CLOSET SAMPLE SALE</strong>, we are the best place in Toronto to show off your goods! Have a look at our <strong>SELL SHEET</strong> below and contact us for pricing and details! </p>
-		<div class="medium default btn icon-left entypo icon-map"><a href="{{ asset('/downloads/C2C_info_sheet_small.pdf') }}">SELL SHEET</a></div>
+		<h3>Be an Exhibitor Now!</h3>
+		<p>Join our team of <strong>prestigious Designer Brands</strong>. For <strong>more information</strong> on the <strong>Catwalk <span class="big_two">2</span> Closet</strong> Event, click below.</p>
+		<div class="medium default btn icon-left entypo icon-newspaper"><a href="{{ asset('/downloads/C2C_info_sheet_small.pdf') }}">Event Details</a></div>
 	</div>
 </div>
