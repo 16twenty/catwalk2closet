3ef96687f898f329a08266eef3bfba061428a9ad
tushar-basu <basu.tushar@gmail.com>
2014-06-13 13:44:15 -0400

    event changes

--- app/views/content/event.blade.php
+++ app/views/content/event.blade.php
@@ -7,7 +7,7 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="six columns cta">
+		<div class="six columns cta_deets">
 			<h3><i class="icon-location"></i>Location</h3>
 			<p>
 				<strong>Heritage Court: Direct Energy Centre, CNE</strong>
@@ -16,7 +16,7 @@
 				<br><a href="https://www.google.ca/maps/place/100+Princes'+Blvd/@43.6344829,-79.4108713,17z/data=!3m1!4b1!4m2!3m1!1s0x882b351a5bca91e9:0xaa5cc333a2007728"><i class="icon-map"></i>Directions</a>
 			</p>
 		</div>
-		<div class="six columns cta_blue">
+		<div class="six columns cta_deets">
 			<h3><i class="icon-calendar"></i>Dates</h3>
 			<p>
 				<strong>Thurs, Oct 23, 2014</strong><span class="pull_right">10:00 AM - 9:00 PM</span>
